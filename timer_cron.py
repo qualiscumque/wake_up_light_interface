177e010c503d89cf6fadc914e05d8f45d6185cb4
Ralf Bottesch <rbottesch@gmail.com>
2018-03-25 12:45:04 +0200

    Updated timer_cron.py to allow exec of alarm scripts

--- timer_cron.py
+++ timer_cron.py
@@ -1,23 +1,35 @@
+import os
 import json
 from datetime import datetime
 from tm1637 import TM1637
 
-# Time as 4-digit int
-current_time = datetime.now().strftime('%H%M')
+# Get current time
+current_time = datetime.now()
+working_dir = os.path.dirname(os.path.abspath(__file__)) + os.sep
 
-# Split int as four individual list entries
-tm = [int(d) for d in str(current_time)]
+def display_time(time_element):
+	time_to_display = time_element.strftime('%H%M')
+	#Display time
+	display = TM1637(CLK=21, DIO=20, brightness=1.0)
+	display.Clear()
+	display.ShowDoublepoint(True)
+	display.Show([int(d) for d in str(time_to_display)])
 
-#Display time
-display = TM1637(CLK=21, DIO=20, brightness=1.0)
-display.Clear()
-display.ShowDoublepoint(True)
-display.Show(tm)
+display_time(current_time)
 
-#alarms = []
-#with open('alarms.json') as alarms_file:    
-#    alarms = json.load(alarms_file)["array"]
-#
-#for alarm_num in range(len(alarms)):
-#    print(alarms[alarm_num]["alarm"])
-#
+alarms = []
+with open(working_dir + 'alarms.json') as alarms_file:    
+    alarms = json.load(alarms_file)["array"]
+
+for alarm_num in range(len(alarms)):
+    alarm_ref_time = current_time.strftime('%H:%M')
+    alarm_entry = alarms[alarm_num]["alarm"]
+    alarm_minutes = ":".join(alarm_entry.split(":")[:2])
+    
+    if(alarm_ref_time == alarm_minutes):
+	action = alarms[alarm_num]["action"]
+	print("running action ", action)
+	os.system("python " + working_dir + action)
+    print(alarm_ref_time)
+    print(alarm_minutes)
+
